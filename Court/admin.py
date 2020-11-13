282a7fc8603cefa24fa4e7df50886561733385fa
Apoorv Garg <u.garg.10@gmail.com>
2020-11-13 10:36:53 +0530

    BasicImplement

--- Court/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Court,Cases
-# Register your models here.
-admin.site.register(Court)
-admin.site.register(Cases)
--- Court/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Court
-# Create your views here.
-def index(request):
-    courts=Court.objects.all()
-    return render(request,"index.html",{'courts':courts})
